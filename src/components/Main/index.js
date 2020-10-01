e9c433f53eb11efafdd88a614a29d35cda763ac8
João Victor <joaovictor-san-co@hotmail.com>
2020-10-01 08:28:04 -0300

    Tentando fazer o React funcionar

---
+++ src/components/Main/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Main = () => (
+  <main>
+    <img src="images/iconfinder_coffee_zorro_avatar_cup_4043245.svg" alt="Coffee-icon" id="logo" />
+    <a href="https://discordapp.com/oauth2/authorize?=&client_id=663894792056602645&scope=bot&permissions=8">
+      <button id="add-bot">Adicionar no servidor</button>
+    </a>
+    <a href="https://discord.gg/hNffyc3">
+      <button id="support-server">Servidor de suporte</button>
+    </a>
+    <a href="comandos.html">
+      <button id="comandos-link">Comandos</button>
+    </a>
+  </main>
+)
+
+export default Main;
